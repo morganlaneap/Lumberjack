07c3b646448775a0b986eee310083d5b7022d014
Yasin Dalal <yasin@dalal.ch>
2020-04-03 17:02:36 +0200

    Added config converter for config-version 1 to 2;

--- src/main/java/com/gageryanplugins/lumberjack/Lumberjack.java
+++ src/main/java/com/gageryanplugins/lumberjack/Lumberjack.java
@@ -4,6 +4,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.util.List;
+import java.util.function.Consumer;
 
 public class Lumberjack extends JavaPlugin implements Listener {
 
@@ -26,11 +28,43 @@ public class Lumberjack extends JavaPlugin implements Listener {
             if (!getDataFolder().exists()) {
                 getDataFolder().mkdirs();
             }
+
             File file = new File(getDataFolder(), "config.yml");
+
             if (!file.exists()) {
                 getLogger().info("Config.yml not found, creating!");
                 saveDefaultConfig();
             } else {
+
+                if(getConfig().getInt("config-version") == 1) {
+
+                    List<String> oldTools = getConfig().getStringList("tools");
+
+                    for(int i = 0; i < oldTools.size(); i++) {
+                        String tool = oldTools.get(i);
+
+                        switch (tool) {
+                            case "wooden":
+                                oldTools.set(i, "WOODEN_AXE");
+                                break;
+                            case "stone":
+                                oldTools.set(i, "STONE_AXE");
+                                break;
+                            case "iron":
+                                oldTools.set(i, "IRON_AXE");
+                                break;
+                            case "golden":
+                                oldTools.set(i, "GOLDEN_AXE");
+                                break;
+                            case "diamond":
+                                oldTools.set(i, "DIAMOND_AXE");
+                                break;
+                        }
+                    }
+
+                    getConfig().set("config-version", 2);
+                }
+
                 getLogger().info("Config.yml found, loading!");
             }
         } catch (Exception e) {
